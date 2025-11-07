30597a81d29d467d60a4b888c80bc75d505e10d1
kik_lenovo_17 <kik87827@gmail.com>
2025-11-07 20:36:22 +0900

    스터디 11/7

---
+++ app/list/DetailLink.js
@@ -0,0 +1,7 @@
+export default function DetailLink() {
+  return (
+    <div>
+      DetailLink
+    </div>
+  );
+}
--- app/list/ListClient.js
+++ app/list/ListClient.js
@@ -1,18 +1,25 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function ListClient({ dataResult }) {
   const router = useRouter();
+  let pathName = usePathname();
+  let searchParams = useSearchParams();
   return (
     <div
       className="list-item"
       key={dataResult._id.toString()}
       onClick={() => router.push(`/detail/${dataResult._id.toString()}`)}
+      prefetch={true}
     >
       <h4>{dataResult.title}</h4>
       <p>{dataResult.content}</p>
       <p>{dataResult.date}</p>
+
+      <p>pathName : {pathName}</p>
+      <p>searchParams : {searchParams}</p>
+
     </div>
   );
 }
